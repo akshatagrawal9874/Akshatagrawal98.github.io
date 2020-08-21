b1a9d01c93c08839a4fb8854efc1f7af7e6f8664
Akshatagrawal98 <65552353+Akshatagrawal98@users.noreply.github.com>
2020-08-21 20:30:13 +0530

    My portfolio website

---
+++ js/app.js
@@ -0,0 +1,80 @@
+$(function(){
+    $(window).on("load resize", function(){
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+    
+    
+    //scroll spy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 150
+    });
+
+    $('nav a').bind('click', function(){
+        $($(this).attr('href')).animatescroll({scrollSpeed: 3000, padding:50});
+        event.preventDefault();
+    })
+
+})
+/*
+$('.progress-bar').each(function(){
+    var bar_value = $(this).attr('aria-valuenow') + '%';
+    $(this).animate({ width: bar_value }, { duration: 2000, easing: 'easeOutCirc' });
+});
+*/
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
